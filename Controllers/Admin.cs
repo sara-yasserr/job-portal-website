b5cc1c42f900db98fe7a645a472dbf335f6024b5
salemgamal <salem.g.salem.m@gmail.com>
2025-04-22 11:13:03 +0200

    Adding 2 Repos

---
+++ Controllers/Admin.cs
@@ -0,0 +1,24 @@
+﻿using Job_Portal_Project.Repositories;
+using Job_Portal_Project.Repositories.ApplicationUserRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Job_Portal_Project.Controllers
+{
+    public class Admin : Controller
+    {   
+        private ICompanyRepository _companyRepository;
+        private IJobRepository _jobRepository;
+        private IJobApplicationRepository _jobApplicationRepository;
+        private IApplicationUserRepository _applicationUserRepository;
+        private IJobCategoryRepository _categoryRepository;
+
+        public Admin()
+        {
+            
+        }
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+    }
+}
---
+++ Repositories/FavouritesRepository.cs
@@ -0,0 +1,48 @@
+﻿using Job_Portal_Project.Models;
+using Job_Portal_Project.Models.DbContext;
+
+namespace Job_Portal_Project.Repositories
+{
+    public class FavouritesRepository : IFavouritesRepository
+    {
+        private JobPortalContext _context;
+        public FavouritesRepository (JobPortalContext context)
+        {
+            _context = context;
+        }
+        public List<Favourites> GetAll()
+        {
+            return _context.Favourites.ToList();
+        }
+
+        public Favourites GetById<I>(I id)
+        {
+            return _context.Favourites.Find(id);
+        }
+
+        public void Insert(Favourites entity)
+        {
+            _context.Favourites.Add(entity);
+        }
+
+        public void Update(Favourites entity)
+        {
+            _context.Favourites.Update(entity);
+        }
+
+        public void Delete<I>(I id)
+        {
+            var Record = _context.Favourites.Find(id);
+            if (Record != null)
+            {
+                _context.Favourites.Remove(Record);
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+}
