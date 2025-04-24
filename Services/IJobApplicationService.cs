4065fdeb7ce3403be694fd6f3db1ccf899f1c0ed
sara-yasserr <sarahyasser979@gmail.com>
2025-04-24 19:15:20 +0200

    add job application view

---
+++ Services/IJobApplicationService.cs
@@ -0,0 +1,13 @@
+﻿using Job_Portal_Project.Models;
+using Job_Portal_Project.ViewModels;
+
+namespace Job_Portal_Project.Services
+{
+    public interface IJobApplicationService : IService<JobApplicationViewModel>
+    {
+        public List<JobApplication> GetUserApplications(string userId);
+        public JobApplication GetJobApplication(int id);
+
+
+    }
+}
