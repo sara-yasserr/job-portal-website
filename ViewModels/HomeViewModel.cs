0e0d36090097337def5b9d0f9e19845910526b55
fatma elshihna <elshinafatma@gmail.com>
2025-05-10 02:17:16 +0300

    latestJob section

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace Job_Portal_Project.ViewModels
+﻿using Job_Portal_Project.Models;
+
+namespace Job_Portal_Project.ViewModels
 {
     public class HomeViewModel
     {
         public List<CategoryWithJobCount> Categories { get; set; }
+        public List<Job> LatestJobs { get; set; } = new();
     }
 }
